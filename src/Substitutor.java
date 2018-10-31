b55e77bbbccc7674bb72f55f6611b39eb61448fe
Freddy M <fredtein@gmail.com>
2018-10-31 17:14:53 +0200

    First class - abstract substitutor.

---
+++ src/Substitutor.java
@@ -0,0 +1,27 @@
+
+public abstract class Substitutor {
+	
+	private int numOfLetters = 26;
+	
+	public abstract void forwardTranslation(char permutation);
+    public abstract void reverseTranslataion(char permutation);
+    public int letterToIndex(char letter){
+    	
+        return (int) (Character.toUpperCase(letter)) - (int)('A');
+        
+    }
+
+    public int leftCircularShift(char letter, int numOfShifts){
+    	
+        return this.rightCircularShift(letter,26-numOfShifts);
+        
+    }
+
+    public int rightCircularShift(char letter, int numOfShifts){
+    	
+        if (numOfShifts<0) return (letter+26+numOfShifts)%26;
+        else return (letter + numOfShifts) % 26;
+        		
+    }
+    
+}
